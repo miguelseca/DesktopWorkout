50ba07bfd04aa47379f36dac1ea9f37bf89d2802
Miguel Seca <miguel.seca@gmail.com>
2023-11-28 18:03:58 +0000

    first real commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,68 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { AfterViewInit, Component, OnInit } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { RouterOutlet } from '@angular/router'
+import { CardComponent } from './components/card/card.component'
+import { EXERCISES } from '../assets/data/exercises'
+import { Exercise } from 'src/models/Exercise'
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+   selector: 'app-root',
+   standalone: true,
+   imports: [CommonModule, RouterOutlet, CardComponent],
+   templateUrl: './app.component.html',
+   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'DesktopWorkout';
+export class AppComponent implements OnInit, AfterViewInit {
+   title = 'Desktop Workout'
+   options = ['Option X', 'Option Y', 'Option Z']
+   minutes: number = 0
+   sound: string = ''
+   showCard: boolean = false
+   isTheAlarmeSet: boolean = false
+   workoutInterval!: any
+   EXERCISES: Exercise[] = EXERCISES;
+   exerciseNumber: number = 0
+   exercise!: Exercise;
+
+   public constructor() {}
+   ngAfterViewInit(): void {}
+   ngOnInit(): void {
+
+
+   console.log(this.EXERCISES);
+   console.log(this.EXERCISES.length);
+
+   this.exercise = EXERCISES[this.exerciseNumber];
+
+
+   }
+
+   setAlarm(sound: string, minutes: string) {
+      this.minutes = parseInt(minutes, 10) * 1000; 
+      this.sound = sound
+
+      if (!this.isTheAlarmeSet) {
+         this.isTheAlarmeSet = true
+
+
+
+         this.workoutInterval = setInterval(() => {
+            this.alarmAwake()
+         }, 1000)
+      }
+   }
+
+   unsetAlarm() {
+      this.isTheAlarmeSet = false
+      this.showCard = false
+      clearInterval(this.workoutInterval)
+   }
+
+   alarmAwake() {
+      let audio: HTMLAudioElement = new Audio('../assets/sounds/mouse.mp3')
+      audio.play()
+      this.exercise = this.EXERCISES[Math.floor(Math.random() * this.EXERCISES.length)];
+
+      this.showCard = true
+   }
 }
---
+++ src/assets/data/exercises.ts
@@ -0,0 +1,48 @@
+export const EXERCISES = [
+   {
+      imageURL: 1,
+      name: 'Seated Marches',
+      description:
+         'Sit on the chair with your back straight. Bend your legs at the knees and “run” on the spot with short, quick steps as fast as you can for 15–20 seconds.',
+   },
+   {
+      imageURL: 2,
+      name: 'Seated Leg Extensions',
+
+      description:
+         'Sit on the edge of your chair with your arms by your sides. Extend your right leg out straight and flex your foot so that just the right heel is on the floor (keeping your foot flexed engages the muscles in the shin and ankle). Lift your leg up as high as you can without rounding your back. Hold for two counts, then lower. Repeat with the other leg. Perform 10 reps on each leg.',
+   },
+   {
+      imageURL: 3,
+      name: 'Seated Reverse Shoulder Fly',
+      description:
+         'Sit on the edge of the chair and lean forward while keeping your lower back naturally arched. Your palms should be facing each other. Raise your arms straight out from your sides. Pause and then slowly return to the starting position. Repeat the exercise 15 times.',
+   },
+
+   {
+      imageURL: 4,
+      name: 'Standing Calf Raises',
+      description:
+         'Stand up behind your chair and hold on for support. Raise your heels off the floor until you are standing on your toes. Slowly lower yourself back to the floor. Perform 12–15 times.',
+   },
+   {
+      imageURL: 5,
+      name: 'Seated Torso Twists',
+      description:
+         'Sit on the edge of the chair, bend your legs at the knees, and lean slightly back without rounding your spine. Bend your arms so your elbows are level with the bottom of your ribcage. Pull your navel in and twist slowly to the left. Inhale and twist to the right. Repeat the exercise dynamically 20 times total.',
+   },
+
+   { imageURL: 6, name: 'Chair Sit and Stand', description: 'tand in front of the chair with your legs shoulder-width apart. Squat down like you are sitting on the chair but without actually touching it. Maintain a proper position: back straight, knees above the feet, weight on the heels. Straighten your legs to go back to the starting position. Repeat the movement 10–15 times.'},
+   { imageURL: 7, name: 'Desk Push-Ups', description: 'While standing, position yourself a few feet from your desk. Keep your feet together. Then, put your palms on the edge of your desk, about a shoulder width away from each other. Lower down to the edge of the desk, and push back to the starting position. Perform 8–15 times. To modify, perform push-up against a wall or on the floor.'},
+   { imageURL: 8, name: 'Standing Leg Curl', description: 'Stand behind your chair and hold onto it for support. Keep your body straight and firm. Start by raising one foot up to your backside and then lowering it down. Repeat the process for 10–15 times, and then switch to the other leg and repeat.' },
+   { imageURL: 9, name: 'Desk Dip', description: 'Put your hands to the edge of the desk, shoulder-width apart. Slide your bottom off the desk and hold yourself up with your arms straight. While keeping your back close to the desk, slowly bend at the elbows and go as low as you can. Return to the starting position. Repeat the exercise 10–15 times'},
+   { imageURL: 10, name: 'Desk Plank', description: 'Place your forearms on the desk, hands touching each other and relaxed. Extend your legs with your toes on the floor. Contract your abdominal muscles. Make sure that you maintain a straight line from your head to toes without lifting or sinking your hips. Hold the position for 10–15 seconds.'},
+   { imageURL: 11, name: 'Seated Hamstring Stretch', description: 'it in your chair with both feet on the ground, then extend one leg outward. Reach toward your toes. Hold stretch for 10–15 seconds while taking deep breaths. Repeat on the other leg for two sets'},
+   { imageURL: 12, name: 'Seated Shoulder Roll', description: 'Raise both shoulders up toward your ears, then slowly roll them backward. Repeat, rolling forward. Sit tall and do not allow your upper back to round. Complete this two times in both directions.'},
+
+   
+
+
+
+
+]
